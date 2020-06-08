f04402191259ed99041e7fa7d6660fc687ab57a6
lvivduncan <lvivduncan@gmail.com>
2020-06-08 17:48:16 +0300

    finished: sum, quantity, clear, add goods

--- js/levus-basket.jquery.js
+++ js/levus-basket.jquery.js
@@ -3,8 +3,18 @@ $('.button').each(function () {
   $(this).click(addToBasket);
 });
 
+// показуємо кількість товарів у кошикові
+viewQuantity();
+
+// сума
+viewSum();
+
+// очистка кошика
+$('#basket-clear').click(clearStorage);
+
 // додаємо у кошик
 function addToBasket() {
+
   // тимчасовий масив з об'єктами
   const content = {
     name: $(this).data('name'),
@@ -14,6 +24,7 @@ function addToBasket() {
 
   // працюємо зі сховищем
   if (localStorage.getItem('basket') === null) {
+
     // пустий масив для додавання даних з кнопок
     const data = [];
 
@@ -23,6 +34,7 @@ function addToBasket() {
     // надсилаємо у сховище
     localStorage.setItem('basket', JSON.stringify(data));
   } else {
+
     // масив з локалстореджа
     const data = JSON.parse(localStorage.getItem('basket'));
 
@@ -32,4 +44,48 @@ function addToBasket() {
     // повертаємо у сховище
     localStorage.setItem('basket', JSON.stringify(data));
   }
+
+  // оновлюємо кількість товарів у кошикові
+  viewQuantity();
+
+  // оновлюємо суму
+  viewSum();
+}
+
+// кількість товарів 
+function viewQuantity() {
+  if (localStorage.getItem('basket')) {
+    $('#basket-quantity').text(JSON.parse(localStorage.getItem('basket')).length);
+  } else {
+    $('#basket-quantity').text(0);
+  }
+}
+
+// сума 
+function viewSum() {
+  let total = 0;
+
+  if (localStorage.getItem('basket')) {
+    JSON.parse(localStorage.getItem('basket'))
+      .map(function (item) {
+        return item.price;
+      })
+      .map(function (sum) {
+        total += +sum;
+        return total;
+      });
+  }
+
+  $('#basket-sum').text(total);
+}
+
+// очистка кошика
+function clearStorage(){
+  localStorage.removeItem('basket');
+
+  // оновлюємо кількість товарів у кошикові
+  viewQuantity();
+  
+  // оновлюємо суму
+  viewSum();
 }
